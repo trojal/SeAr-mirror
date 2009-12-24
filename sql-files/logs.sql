03a135bf3be096de4b1001574d790539b68c6090
Inkfish <Inkfish@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2009-12-24 09:34:51 +0000

    2009/12/24[SeAr_1.3.37]
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@227 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

--- sql-files/logs.sql
+++ sql-files/logs.sql
@@ -124,3 +124,11 @@ CREATE TABLE `loginlog` (
   `log` varchar(255) NOT NULL default '',
   INDEX (`ip`)
 ) ENGINE=MyISAM ;
+
+#Database: log
+#Table: usercountlog
+CREATE TABLE `usercountlog` (
+  `index` tinyint(4) unsigned NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  `user` int(11) unsigned NOT NULL default '0'
+) ENGINE=MyISAM;
---
+++ sql-files/upgrade_r1.3.37.sql
@@ -0,0 +1,9 @@
+#Database: log
+#Table: usercountlog
+CREATE TABLE `usercountlog` (
+  `index` tinyint(4) unsigned NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  `user` int(11) unsigned NOT NULL default '0'
+) ENGINE=MyISAM;
+
+
