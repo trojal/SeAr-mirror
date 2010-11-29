df8c358d6b46c85c408df172dc3b36d5f3644e32
Inkfish <Inkfish@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2010-11-29 14:57:28 +0000

    2010/11/29[SeAr_1.5.126]
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@634 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

--- sql-files/logs.sql
+++ sql-files/logs.sql
@@ -37,6 +37,21 @@ CREATE TABLE `zenylog` (
   INDEX (`type`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 ;
 
+#CashLog types (C)ash Points,(K)afra Points
+#Database: log
+#Table: cashlog
+CREATE TABLE `cashlog` (
+  `id` int(11) NOT NULL auto_increment,
+  `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `char_id` int(11) NOT NULL default '0',
+  `type` enum('C','K') NOT NULL default 'C',
+  `amount` int(11) NOT NULL default '0',
+  `remain` int(11) NOT NULL default '0',
+  `map` varchar(11) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  INDEX (`type`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 ;
+
 #Database: log
 #Table: branchlog
 CREATE TABLE `branchlog` (
