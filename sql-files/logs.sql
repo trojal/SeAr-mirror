52b880d6c5d702fd88a413c7b612c5754ed04a3a
Inkfish <Inkfish@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2010-12-04 03:34:55 +0000

    2010/12/04[SeAr_1.5.130]
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@638 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

--- sql-files/logs.sql
+++ sql-files/logs.sql
@@ -8,6 +8,7 @@
 CREATE TABLE `picklog` (
   `id` int(11) NOT NULL auto_increment,
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `account_id` int(11) NOT NULL default '0',
   `char_id` int(11) NOT NULL default '0',
   `type` enum('M','P','L','T','V','S','N','C','A','R','G','E') NOT NULL default 'P',
   `nameid` int(11) NOT NULL default '0',
@@ -28,6 +29,7 @@ CREATE TABLE `picklog` (
 CREATE TABLE `zenylog` (
   `id` int(11) NOT NULL auto_increment,
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `account_id` int(11) NOT NULL default '0',
   `char_id` int(11) NOT NULL default '0',
   `src_id` int(11) NOT NULL default '0',
   `type` enum('M','T','V','S','N','A','E') NOT NULL default 'S',
@@ -43,6 +45,7 @@ CREATE TABLE `zenylog` (
 CREATE TABLE `cashlog` (
   `id` int(11) NOT NULL auto_increment,
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `account_id` int(11) NOT NULL default '0',
   `char_id` int(11) NOT NULL default '0',
   `type` enum('C','K') NOT NULL default 'C',
   `amount` int(11) NOT NULL default '0',
