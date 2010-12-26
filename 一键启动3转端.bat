c8bafde735e1b229665700e19f4e76e231986bcf
Inkfish <Inkfish@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2010-12-26 13:00:07 +0000

    2010/12/26[SeAr_1.5.160]
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@668 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

---
+++ 一键启动3转端.bat
@@ -0,0 +1,13 @@
+@echo off
+cd conf/battle
+if exist Renewal_.conf del Renewal_.conf
+copy Renewal.conf Renewal_temp.conf
+cd ../../npc
+if exist scripts_monsters.conf del scripts_monsters.conf
+if exist scripts_warps.conf del scripts_warps.conf
+copy scripts_monsters_renewal.conf scripts_monsters.conf
+copy scripts_warps_renewal.conf scripts_warps.conf
+cd ..
+start cmd /k map-server.bat
+start cmd /k char-server.bat
+start cmd /k login-server.bat
