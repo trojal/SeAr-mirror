5e8296dcf62710d7379cde552ed1c349b8457f66
Inkfish <Inkfish@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2011-03-24 12:19:29 +0000

    2011/03/24[SeAr_1.6.62]
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@768 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

---
+++ sql-files/upgrade_r1.6.62.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `char` MODIFY COLUMN `hp` int(11) NOT NULL default '0';
+ALTER TABLE `char` MODIFY COLUMN `sp` int(11) NOT NULL default '0';
+ALTER TABLE `char` MODIFY COLUMN `max_hp` int(11) NOT NULL default '0';
+ALTER TABLE `char` MODIFY COLUMN `max_sp` int(11) NOT NULL default '0';
