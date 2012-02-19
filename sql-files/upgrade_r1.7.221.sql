f50ba6d7154f5e81ce793e24b5efb727c0c42509
Inkfish <Inkfish@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2012-02-19 07:57:14 +0000

    2012/02/19[SeAr_1.7.222]
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@1033 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

---
+++ sql-files/upgrade_r1.7.221.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `cart_inventory` ADD `favorite` tinyint(2) UNSIGNED NOT NULL default '0';
+ALTER TABLE `guild_storage` ADD `favorite` tinyint(2) UNSIGNED NOT NULL default '0';
+ALTER TABLE `inventory` ADD `favorite` tinyint(2) UNSIGNED NOT NULL default '0';
+ALTER TABLE `storage` ADD `favorite` tinyint(2) UNSIGNED NOT NULL default '0';
