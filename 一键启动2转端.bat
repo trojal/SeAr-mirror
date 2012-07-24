34c562562740ad3459afd031004715e474ac7d0e
yuxiangshang <yuxiangshang@7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18>
2012-07-24 12:40:39 +0000

    2012/07/24[SeAr_1.8.16] - DB:		修正pre-renewal读取物品相关数据时的警告。(by 5511) - NPC:		将merchants分划出merchants_prerenewal。(by 5511) - NPC:		将SeA目录中renewal相关内容划分并移至merchants。(by 5511) - NPC:		中文化部分商人。(by 5511) - NPC:		修正并更新shops.txt。(by 5511) - NPC:		更新并添加部分商人的位置坐标。(by 5511) - NPC:		更新renewal精炼师的几率判断（重要，详见相关文件）。(by 5511) - NPC:		修正符文石贩卖商的普通符文石的ID错误。(by 5511)
    git-svn-id: http://my-svn.assembla.com/svn/SeAr@1157 7aba9ba3-0ac2-4fee-b7b5-ab11d9180d18

--- 一键启动2转端.bat
+++ 一键启动2转端.bat
@@ -6,8 +6,10 @@ cd ../import
 if exist renewal_temp.txt del renewal_temp.txt
 copy prerenewal.txt renewal_temp.txt
 cd ../../npc
+if exist scripts_athena.conf del scripts_athena.conf
 if exist scripts_monsters.conf del scripts_monsters.conf
 if exist scripts_warps.conf del scripts_warps.conf
+copy scripts_athena_prerenewal.conf scripts_athena.conf
 copy scripts_monsters_prerenewal.conf scripts_monsters.conf
 copy scripts_warps_prerenewal.conf scripts_warps.conf
 cd ..
